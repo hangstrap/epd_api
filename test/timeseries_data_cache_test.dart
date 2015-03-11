d18f2d48aa1544d0d54a99e3a1e66c1d8e8a904e
hangstrap <hangstrap@gmail.com>
2015-03-11 21:41:18 +1300

    wip

--- test/timeseries_data_cache_test.dart
+++ test/timeseries_data_cache_test.dart
@@ -1,33 +1,46 @@
 import 'package:unittest/unittest.dart';
+import 'package:mock/mock.dart';
+
 import '../bin/timeseries_data_cache.dart';
 import 'dart:async';
 import 'package:quiver/time.dart';
 
-void main(){
-  
-  test( "basic", (){
-    
-    Future<TimeseriesAssembly> loadTimeseries (TimeseriesRootAnalysis key){
-      
-      print( "inside loadTimeseries");
+@proxy
+class MockTimeseriesAssemply extends Mock implements TimeseriesAssembly{}
 
-      return null;
+void main() {
+
+
+
+  
+  test("basic", () {
+    
+    MockTimeseriesAssemply assembly = new MockTimeseriesAssemply();
+
+    Future<TimeseriesAssembly> loadTimeseries(TimeseriesRootAnalysis key) {
+      print("inside loadTimeseries");      
+      return new Future.value( assembly);
     }
-    
-    
-    TimeseriesDataCache cache = new TimeseriesDataCache( loadTimeseries);
-    
-    TimeseriesRootAnalysis key;
-    Clock clock  = new Clock();
-    
-    return new Future.value().then((_) {
-      Future<TimeseriesAssembly> result = cache.getTimeseries(key, clock.now(), new Duration());
-      expect( result, isNotNull);
-         });
 
-    
-    
-    
+
+    TimeseriesDataCache cache = new TimeseriesDataCache(loadTimeseries);
+
+    TimeseriesRootAnalysis key;
+    Clock clock = new Clock();
+
+    return new Future.value().then((_) {
+      Future<TimeseriesAssembly> future = cache.getTimeseries(key, clock.now(), new Duration());
+      expect(future, isNotNull);
+      future.then( (TimeseriesAssembly a){
+        
+        expect( a, isNotNull);
+        expect( a, same( assembly));
+      } );
+    });
+
+
+
+
   });
-  
+
 }
