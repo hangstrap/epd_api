ad37624f8869f86e5b0ba77285278d24b8b18dd4
Richard Schmidt <hangstrap@gmail.com>
2015-03-26 22:18:02 +1300

    wip

--- bin/server.dart
+++ bin/server.dart
@@ -4,7 +4,36 @@
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
 
+import "timeseries_data_cache.dart";
+import "caf_file_retriever.dart";
+import "timeseries_model.dart";
+import "dart:async";
+
 void main(List<String> args) {
+
+  CafFileRetriever retriever = new CafFileRetriever( "data");
+  TimeseriesDataCache cache = new TimeseriesDataCache( retriever.loadTimeseres);
+
+  
+  Future<shelf.Response> _echoRequest(shelf.Request request) {
+     
+    TimeseriesAnalysis analysis = new TimeseriesAnalysis(
+        new Product("City Town & Spot Forecasts"), 
+        new Model("PDF-PROFOUND"), 
+        new DateTime.utc(2015, 02, 15, 03, 00), 
+        new Element("TTTTT"), 
+        new Location("01492", "INTL"));
+    
+    Future<TimeseriesAssembly>  futureAssembly = cache.getTimeseries( analysis, null, null);
+    
+    
+    return futureAssembly.then( (TimeseriesAnalysis assembly) => new shelf.Response.ok( assembly.toString()); 
+  }
+
+  
+  
+  
+  
   
   var port = 8080;
   var handler = const shelf.Pipeline()
@@ -14,8 +43,6 @@ void main(List<String> args) {
   io.serve(handler, 'localhost', port).then((server) {
     print('Serving at http://${server.address.host}:${server.port}');
   });
-}
 
-shelf.Response _echoRequest(shelf.Request request) {
-  return new shelf.Response.ok('Request for "${request.url}"');
+
 }
