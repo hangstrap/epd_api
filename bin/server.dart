3c50c5149d53ef0c977a94b3cbaa4bf7602d6ea9
hangstrap <hangstrap@gmail.com>
2015-03-27 17:12:44 +1300

    wip

--- bin/server.dart
+++ bin/server.dart
@@ -9,36 +9,44 @@ import "caf_file_retriever.dart";
 import "timeseries_model.dart";
 import "dart:async";
 
+import 'package:jsonx/jsonx.dart';
+
 void main(List<String> args) {
 
-  CafFileRetriever retriever = new CafFileRetriever( "data");
-  TimeseriesDataCache cache = new TimeseriesDataCache( retriever.loadTimeseres);
+  CafFileRetriever retriever = new CafFileRetriever("../data");
+  TimeseriesDataCache cache = new TimeseriesDataCache(retriever.loadTimeseres);
 
-  
+
   Future<shelf.Response> _echoRequest(shelf.Request request) {
-     
+
     TimeseriesAnalysis analysis = new TimeseriesAnalysis(
         new Product("City Town & Spot Forecasts"), 
         new Model("PDF-PROFOUND"), 
         new DateTime.utc(2015, 02, 15, 03, 00), 
         new Element("TTTTT"), 
         new Location("01492", "INTL"));
-    
-    Future<TimeseriesAssembly>  futureAssembly = cache.getTimeseries( analysis, null, null);
-    
-    
-    return futureAssembly.then( (TimeseriesAnalysis assembly) => new shelf.Response.ok( assembly.toString()); 
+
+    Future<TimeseriesAssembly> futureAssembly = cache.getTimeseries(analysis, null, null);
+
+
+    return futureAssembly.then((TimeseriesAssembly assembly) {
+
+
+      //convert to json
+      String json = encode( assembly);
+
+      return new shelf.Response.ok(json);
+
+    });
   }
 
-  
-  
-  
-  
-  
+
+
+
+
+
   var port = 8080;
-  var handler = const shelf.Pipeline()
-      .addMiddleware(shelf.logRequests())
-      .addHandler(_echoRequest);
+  var handler = const shelf.Pipeline().addMiddleware(shelf.logRequests()).addHandler(_echoRequest);
 
   io.serve(handler, 'localhost', port).then((server) {
     print('Serving at http://${server.address.host}:${server.port}');
