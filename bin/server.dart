6deecaa6242b7c1bd446ae10236f2db6aaa7e705
hangstrap <hangstrap@gmail.com>
2015-03-31 21:49:05 +1300

    basic api runing

--- bin/server.dart
+++ bin/server.dart
@@ -9,46 +9,68 @@ import "caf_file_retriever.dart";
 import "timeseries_model.dart";
 import "dart:async";
 
+
 import 'package:jsonx/jsonx.dart';
 
+///epd/byAnalysis/product/model/201502150300Z?locations=01492.INTL,&elements=TTTTT
+
+List<TimeseriesNode> extractNodes(List<String> pathSegments, Map<String, String> queryParams){
+  
+  String product = pathSegments[2];
+  String model = pathSegments[3];
+  
+  List<TimeseriesNode> result = [];
+  List<String> locations = queryParams["locations"].split(",");
+  List<String> elements = queryParams["elements"].split( ",");
+  
+  locations.forEach((location){
+    elements.forEach((element) {
+      String locationName = location.split("\.")[0];
+      String locationSuffix = location.split("\.")[1];      
+      result.add( new TimeseriesNode(product, model, element, locationName, locationSuffix));
+    });
+  });
+  
+  return result;
+}
+ 
 void main(List<String> args) {
 
   CafFileRetriever retriever = new CafFileRetriever("data");
   TimeseriesDataCache cache = new TimeseriesDataCache(retriever.loadTimeseres);
 
 
-  Future<shelf.Response> _echoRequest(shelf.Request request) {
+  Future<shelf.Response> processRequest(shelf.Request request) {
 
-    TimeseriesNode node = new TimeseriesNode("City Town & Spot Forecasts", "PDF-PROFOUND", "TTTTT", "01492", "INTL");
+    List<String> pathSegments = request.url.pathSegments;
+    if(( pathSegments.length != 5)|| ( pathSegments[0] != "epd") || ( pathSegments[1] != "byAnalysis")){
+      
+      return new Future.value( new shelf.Response.internalServerError(
+          body:'url must be like /epd/byAnalysis/City Town & Spot Forecasts/PDF-PROFOUND/20150215T0300Z?locations=01492.INTL,03266.INTL&elements=TTTTT'));
+    }
+  
+    DateTime analysis = DateTime.parse( pathSegments[4]);
+    List<TimeseriesNode> nodes = extractNodes( pathSegments, request.url.queryParameters);
+    
+    Future<List<TimeseriesAssembly>> futureAssembly = cache.getTimeseriesAnalysisSet(nodes, analysis, null, null);
 
-    DateTime analysis = new DateTime.utc(2015, 02, 15, 03, 00);
 
-
-    Future<TimeseriesAssembly> futureAssembly = cache.getTimeseriesAnalysis(node, analysis, null, null);
-
-
-    return futureAssembly.then((TimeseriesAssembly assembly) {
-
+    return futureAssembly.then((assembly) {
 
       //convert to json
       String json = encode(assembly);
-
       return new shelf.Response.ok(json);
 
     });
   }
 
 
-
-
-
-
   var port = 8080;
-  var handler = const shelf.Pipeline().addMiddleware(shelf.logRequests()).addHandler(_echoRequest);
+  var handler = const shelf.Pipeline().addMiddleware(shelf.logRequests()).addHandler(processRequest);
 
   io.serve(handler, 'localhost', port).then((server) {
     print('Serving at http://${server.address.host}:${server.port}');
   });
-
-
 }
+
+
