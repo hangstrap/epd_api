693771697e12fd311ccf1ee733c8eea45567d94a
hangstrap <hangstrap@gmail.com>
2015-04-11 21:18:20 +1200

    wip

--- bin/web_site_listing_crawler.dart
+++ bin/web_site_listing_crawler.dart
@@ -9,7 +9,7 @@ import 'package:quiver/async.dart';
 
 class Link {
   final Uri _baseUrl;
-  final parser.Item _item;
+  final parser.Item _item;x
   Link(this._baseUrl, this._item);
 
   Uri get url => _baseUrl.resolve(_item.uri);
