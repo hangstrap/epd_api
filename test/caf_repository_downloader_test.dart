e788079c6164ada1eadf1aaf64fcd978c8001b5d
hangstrap <hangstrap@gmail.com>
2015-04-13 20:40:16 +1200

    wip

--- test/caf_repository_downloader_test.dart
+++ test/caf_repository_downloader_test.dart
@@ -1,9 +1,16 @@
 import 'package:unittest/unittest.dart';
 
 import 'package:intl/intl.dart';
+import 'dart:io';
+import 'package:http_server/http_server.dart' show VirtualDirectory;
 
 
 main(){
+  
+  setUp((){
+    startTestServer();  
+  });
+
   test( "Date formatter",(){
     
     //27-Mar-2015 22:13
@@ -13,3 +20,15 @@ main(){
     
   });
 }
+
+
+void startTestServer() {
+  final MY_HTTP_ROOT_PATH = Platform.script.resolve('www').toFilePath();
+  final virDir = new VirtualDirectory(MY_HTTP_ROOT_PATH)
+    ..allowDirectoryListing = true;
+
+  HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080).then((server) {
+    server.listen((request) {
+      virDir.serveRequest(request);
+    });
+  });
