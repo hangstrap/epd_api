1afe5116ef9e827f9fce61af2374ffe1d87ba725
hangstrap <hangstrap@gmail.com>
2015-04-13 20:25:06 +1200

    untested downloader

--- bin/caf_repository_downloader.dart
+++ bin/caf_repository_downloader.dart
@@ -1,30 +1,51 @@
 library caf_repository_downloader;
 import 'web_site_listing_crawler.dart' as crawler;
+import 'caf_file_decoder.dart' as deconder;
 import "dart:io";
 import 'dart:core';
 import 'package:intl/intl.dart';
-
-
-void downloaderCafFilesFromWebSite(Uri url,Directory destination, DateTime lastDownloaded){
-  
-  DateTime convertDate( String dateStr){
-    //27-Mar-2015 22:13
-    new DateFormat( )
-  }
+import 'package:http/http.dart' as http;
+import 'dart:async';
   
 
-  void downloadIfNecessary( crawler.Link link){
-    
-  }
-
+Future<DateTime> downloaderCafFilesFromWebSite(Uri url,Directory destination, DateTime timeOfLastFileDownloaded){
+  
+  DateFormat df = new DateFormat( "d-MMM-y HH:mm");
+  DateTime latestLastModifiedTime = timeOfLastFileDownloaded;
+ 
   bool foundLink(crawler.Link link){
+
+    DateTime lastModified = df.parse( link.lastModifiedAt);
+    if(lastModified.isBefore( timeOfLastFileDownloaded)){
+      return false;
+    }
+   if( latestLastModifiedTime.isBefore( lastModified)){
+     latestLastModifiedTime = lastModified;
+   }
+   if( link.isDirectory){
+     
+        return true;
+    }
     
     if( link.name.endsWith( '.caf')){
-      downloadIfNecessary( link);
+
+      http.get( link.url).then((response){
+        
+        String contents = response.body;
+        String fileName = deconder.fileNameForCafFile( contents.split("\n"));
+        File file = new File( destination.path + fileName);
+        
+        Directory newParent = file.parent;
+        if( !newParent.existsSync()){
+          newParent.createSync(recursive:true);
+        }
+
+        file.writeAsString(contents).then((file)=>print( "downloaded caf file ${file}"));
+      });
     } 
     return true;
   }
 
-  
-
+   
+  return crawler.crawl(url, foundLink).then( (_) => latestLastModifiedTime);
 }
