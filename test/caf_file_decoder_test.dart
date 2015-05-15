18cfd262b849340668f255a3a4f49d66d9d71013
hangstrap <hangstrap@gmail.com>
2015-05-15 16:39:21 +1200

    change file path

--- test/caf_file_decoder_test.dart
+++ test/caf_file_decoder_test.dart
@@ -5,7 +5,6 @@ import 'matchers.dart';
 import '../bin/caf_file_decoder.dart' as caf;
 import '../bin/timeseries_model.dart';
 
-
 @proxy
 class MockTimeseriesNode extends Mock implements TimeseriesNode {}
 
@@ -13,27 +12,29 @@ main() {
   group("create correct file name based on CAF file contents", () {
     test("check the pattern for a 99xxx station", () {
       String cafFileName = caf.fileNameForCafFile(cafFileHeader99xxxx);
-      expect(cafFileName, equals("CityTownSpotForecasts/PDF-PROFOUND/201502150300Z/TTTTT/CityTownSpotForecasts.PDF-PROFOUND.201502150300Z.TTTTT.99647-INTL.caf"));
+      expect(cafFileName, equals(
+          "CityTownSpotForecasts/PDF-PROFOUND/TTTTT/99647-INTL/201502150300Z/CityTownSpotForecasts.PDF-PROFOUND.TTTTT.201502150300Z.99647-INTL.caf"));
     });
     test("check the pattern for a non 99xxx station", () {
       String cafFileName = caf.fileNameForCafFile(cafFileHeader123456);
-      expect(cafFileName, equals("CityTownSpotForecasts/PDF-PROFOUND/201502150300Z/TTTTT/CityTownSpotForecasts.PDF-PROFOUND.201502150300Z.TTTTT.123456.caf"));
+      expect(cafFileName, equals(
+          "CityTownSpotForecasts/PDF-PROFOUND/TTTTT/123456/201502150300Z/CityTownSpotForecasts.PDF-PROFOUND.TTTTT.201502150300Z.123456.caf"));
     });
   });
 
   group("create correct file name based on TimeseriesAnalysis", () {
     test("check pattern for a 99xxx station", () {
-
       DateTime analysis = new DateTime.utc(2015, 2, 15, 3, 0);
-      TimeseriesNode node = new TimeseriesNode.create("City, Town & Spot Forecasts", "PDF-PROFOUND", "TTTTT", "99647", "INTL");
+      TimeseriesNode node =
+          new TimeseriesNode.create("City, Town & Spot Forecasts", "PDF-PROFOUND", "TTTTT", "99647", "INTL");
 
       String cafFileName = caf.fileNameForTimeseriesAnalysis(node, analysis);
-      expect(cafFileName, equals("CityTownSpotForecasts/PDF-PROFOUND/201502150300Z/TTTTT/CityTownSpotForecasts.PDF-PROFOUND.201502150300Z.TTTTT.99647-INTL.caf"));
+      expect(cafFileName, equals(
+          "CityTownSpotForecasts/PDF-PROFOUND/TTTTT/99647-INTL/201502150300Z/CityTownSpotForecasts.PDF-PROFOUND.TTTTT.201502150300Z.99647-INTL.caf"));
     });
   });
 
   group("caf file parser", () {
-
     List<String> lines;
     group("break into blocks", () {
       setUp(() {
@@ -51,7 +52,6 @@ main() {
 
         expect(result[1][0], equals("four"));
         expect(result[1][1], equals("five"));
-
       }
       test("should handle no blank line at end", () {
         checkResult(caf.breakIntoCafBlocks(lines));
@@ -68,8 +68,6 @@ main() {
 
     group("parse of header block", () {
       test("parse of 99XXXX header", () {
-
-
         TimeseriesNode analysis = caf.toTimeseriesNode(cafFileHeader99xxxx);
 
         check99647Node(analysis);
@@ -90,35 +88,31 @@ main() {
       lines.addAll(cafFileHeader99xxxx);
       lines.addAll(cafFileBlock);
 
-
       test("", () {
-
         TimeseriesAssembly assembly = caf.toTimeseiesAssembly(lines);
         check99647Node(assembly.node);
-        DateTime analysis  = new DateTime.utc(2015, 2, 15, 3, 00);
-        expect( assembly.analysis, equals( analysis));
-        checkEdition( assembly.editions[0], analysis);
+        DateTime analysis = new DateTime.utc(2015, 2, 15, 3, 00);
+        expect(assembly.analysis, equals(analysis));
+        checkEdition(assembly.editions[0], analysis);
       });
     });
-    
-    test( "throw meaningful error message when token missing", (){
-      List<String> lines = ["aaaa=aaaa,bbbb=bbbb"];
-      expect(() =>  caf.toTimeseriesNode( lines),  throwsA( exceptionMatching( FormatException,  "Could not find token 'product'"))); 
 
+    test("throw meaningful error message when token missing", () {
+      List<String> lines = ["aaaa=aaaa,bbbb=bbbb"];
+      expect(() => caf.toTimeseriesNode(lines),
+          throwsA(exceptionMatching(FormatException, "Could not find token 'product'")));
     });
   });
-
 }
 
-void checkEdition( Edition edition,  DateTime analysis){
-
+void checkEdition(Edition edition, DateTime analysis) {
   expect(edition.analysisAt, equals(analysis));
   expect(edition.validFrom, equals(analysis.add(new Duration(hours: 1))));
   expect(edition.validTo, equals(analysis.add(new Duration(hours: 1))));
   expect(edition.datum['mean'], equals(5.516087));
-  expect(edition.datum['control-points'], equals([1.587835,4.687500,5.524008]));
-  expect(edition.datum['logn-pdf-values'], equals([-6.502432,-1.297167,-0.893834]));  
-  expect(edition.datum['curvature-values'], equals([0.416317,0.094229,0.000001]));
+  expect(edition.datum['control-points'], equals([1.587835, 4.687500, 5.524008]));
+  expect(edition.datum['logn-pdf-values'], equals([-6.502432, -1.297167, -0.893834]));
+  expect(edition.datum['curvature-values'], equals([0.416317, 0.094229, 0.000001]));
   expect(edition.datum['tail-left'], equals(1));
   expect(edition.datum['tail-right'], equals(1));
   expect(edition.datum['variance'], equals(1.111753));
@@ -153,7 +147,6 @@ station:=123456
 station-99suffix:=INTL
 init-time:=20150215 0300 Z
 """.split("\n");
-
 
 List<String> cafFileBlock = """prog=1h
 control-points=1.587835,4.687500,5.524008
