1b2440ec79e7b77e8ea5ca6bf3536e28adb0ef06
Richard Schmidt <hangstrap@gmail.com>
2015-07-01 21:41:56 +1200

    missing files

---
+++ lib/client/epd.dart
@@ -0,0 +1,346 @@
+// This is a generated file (see the discoveryapis_generator project).
+
+library epd_api_shelf.epd.client;
+
+import 'dart:core' as core;
+import 'dart:async' as async;
+
+
+import 'package:_discoveryapis_commons/_discoveryapis_commons.dart' as commons;
+import 'package:http/http.dart' as http;
+import 'package:epd_api_shelf/common/timeseries_model.dart';
+import 'package:epd_api_shelf/server/epd_api.dart';
+export 'package:_discoveryapis_commons/_discoveryapis_commons.dart' show
+    ApiRequestError, DetailedApiRequestError;
+
+const core.String USER_AGENT = 'dart-api-client epd/v1';
+
+/** Epd Api */
+class Epd {
+
+  final commons.ApiRequester _requester;
+
+  Epd(http.Client client, {core.String rootUrl: "http://localhost:8080/", core.String servicePath: "epd/v1/"}) :
+      _requester = new commons.ApiRequester(client, rootUrl, servicePath, USER_AGENT);
+
+  /**
+   * Request parameters:
+   *
+   * [product] - Path parameter: 'product'.
+   *
+   * [model] - Path parameter: 'model'.
+   *
+   * [analysis] - Path parameter: 'analysis'.
+   *
+   * [locations] - Query parameter: 'locations'.
+   *
+   * [elements] - Query parameter: 'elements'.
+   *
+   * [validFrom] - Query parameter: 'validFrom'.
+   *
+   * [validTo] - Query parameter: 'validTo'.
+   *
+   * Completes with a [core.List<TimeseriesAssembly>].
+   *
+   * Completes with a [commons.ApiRequestError] if the API endpoint returned an
+   * error.
+   *
+   * If the used [http.Client] completes with an error when making a REST call,
+   * this method will complete with the same error.
+   */
+  async.Future<core.List<TimeseriesAssembly>> byAnalysis(core.String product, core.String model, core.String analysis, {core.String locations, core.String elements, core.String validFrom, core.String validTo}) {
+    var _url = null;
+    var _queryParams = new core.Map();
+    var _uploadMedia = null;
+    var _uploadOptions = null;
+    var _downloadOptions = commons.DownloadOptions.Metadata;
+    var _body = null;
+
+    if (product == null) {
+      throw new core.ArgumentError("Parameter product is required.");
+    }
+    if (model == null) {
+      throw new core.ArgumentError("Parameter model is required.");
+    }
+    if (analysis == null) {
+      throw new core.ArgumentError("Parameter analysis is required.");
+    }
+    if (locations != null) {
+      _queryParams["locations"] = [locations];
+    }
+    if (elements != null) {
+      _queryParams["elements"] = [elements];
+    }
+    if (validFrom != null) {
+      _queryParams["validFrom"] = [validFrom];
+    }
+    if (validTo != null) {
+      _queryParams["validTo"] = [validTo];
+    }
+
+    _url = 'byAnalysis/' + commons.Escaper.ecapeVariable('$product') + '/' + commons.Escaper.ecapeVariable('$model') + '/' + commons.Escaper.ecapeVariable('$analysis');
+
+    var _response = _requester.request(_url,
+                                       "GET",
+                                       body: _body,
+                                       queryParams: _queryParams,
+                                       uploadOptions: _uploadOptions,
+                                       uploadMedia: _uploadMedia,
+                                       downloadOptions: _downloadOptions);
+    return _response.then((data) => data.map((value) => TimeseriesAssemblyFactory.fromJson(value)).toList());
+  }
+
+  /**
+   * Request parameters:
+   *
+   * [product] - Path parameter: 'product'.
+   *
+   * [model] - Path parameter: 'model'.
+   *
+   * [validFrom] - Path parameter: 'validFrom'.
+   *
+   * [validTo] - Path parameter: 'validTo'.
+   *
+   * [locations] - Query parameter: 'locations'.
+   *
+   * [elements] - Query parameter: 'elements'.
+   *
+   * Completes with a [core.List<TimeseriesBestSeries>].
+   *
+   * Completes with a [commons.ApiRequestError] if the API endpoint returned an
+   * error.
+   *
+   * If the used [http.Client] completes with an error when making a REST call,
+   * this method will complete with the same error.
+   */
+  async.Future<core.List<TimeseriesBestSeries>> byLatest(core.String product, core.String model, core.String validFrom, core.String validTo, {core.String locations, core.String elements}) {
+    var _url = null;
+    var _queryParams = new core.Map();
+    var _uploadMedia = null;
+    var _uploadOptions = null;
+    var _downloadOptions = commons.DownloadOptions.Metadata;
+    var _body = null;
+
+    if (product == null) {
+      throw new core.ArgumentError("Parameter product is required.");
+    }
+    if (model == null) {
+      throw new core.ArgumentError("Parameter model is required.");
+    }
+    if (validFrom == null) {
+      throw new core.ArgumentError("Parameter validFrom is required.");
+    }
+    if (validTo == null) {
+      throw new core.ArgumentError("Parameter validTo is required.");
+    }
+    if (locations != null) {
+      _queryParams["locations"] = [locations];
+    }
+    if (elements != null) {
+      _queryParams["elements"] = [elements];
+    }
+
+    _url = 'byLatest/' + commons.Escaper.ecapeVariable('$product') + '/' + commons.Escaper.ecapeVariable('$model') + '/' + commons.Escaper.ecapeVariable('$validFrom') + '/' + commons.Escaper.ecapeVariable('$validTo');
+
+    var _response = _requester.request(_url,
+                                       "GET",
+                                       body: _body,
+                                       queryParams: _queryParams,
+                                       uploadOptions: _uploadOptions,
+                                       uploadMedia: _uploadMedia,
+                                       downloadOptions: _downloadOptions);
+    return _response.then((data) => data.map((value) => TimeseriesBestSeriesFactory.fromJson(value)).toList());
+  }
+
+  /**
+   * Request parameters:
+   *
+   * Completes with a [MyMessage].
+   *
+   * Completes with a [commons.ApiRequestError] if the API endpoint returned an
+   * error.
+   *
+   * If the used [http.Client] completes with an error when making a REST call,
+   * this method will complete with the same error.
+   */
+  async.Future<MyMessage> index() {
+    var _url = null;
+    var _queryParams = new core.Map();
+    var _uploadMedia = null;
+    var _uploadOptions = null;
+    var _downloadOptions = commons.DownloadOptions.Metadata;
+    var _body = null;
+
+
+    _url = 'index';
+
+    var _response = _requester.request(_url,
+                                       "GET",
+                                       body: _body,
+                                       queryParams: _queryParams,
+                                       uploadOptions: _uploadOptions,
+                                       uploadMedia: _uploadMedia,
+                                       downloadOptions: _downloadOptions);
+    return _response.then((data) => MyMessageFactory.fromJson(data));
+  }
+
+}
+
+
+
+class EditionFactory {
+  static Edition fromJson(core.Map _json) {
+    var message = new Edition();
+    if (_json.containsKey("analysisAt")) {
+      message.analysisAt = core.DateTime.parse(_json["analysisAt"]);
+    }
+    if (_json.containsKey("datum")) {
+      message.datum = _json["datum"];
+    }
+    if (_json.containsKey("validFrom")) {
+      message.validFrom = core.DateTime.parse(_json["validFrom"]);
+    }
+    if (_json.containsKey("validTo")) {
+      message.validTo = core.DateTime.parse(_json["validTo"]);
+    }
+    return message;
+  }
+
+  static core.Map toJson(Edition message) {
+    var _json = new core.Map();
+    if (message.analysisAt != null) {
+      _json["analysisAt"] = (message.analysisAt).toIso8601String();
+    }
+    if (message.datum != null) {
+      _json["datum"] = message.datum;
+    }
+    if (message.validFrom != null) {
+      _json["validFrom"] = (message.validFrom).toIso8601String();
+    }
+    if (message.validTo != null) {
+      _json["validTo"] = (message.validTo).toIso8601String();
+    }
+    return _json;
+  }
+}
+
+class MyMessageFactory {
+  static MyMessage fromJson(core.Map _json) {
+    var message = new MyMessage();
+    if (_json.containsKey("message")) {
+      message.message = _json["message"];
+    }
+    return message;
+  }
+
+  static core.Map toJson(MyMessage message) {
+    var _json = new core.Map();
+    if (message.message != null) {
+      _json["message"] = message.message;
+    }
+    return _json;
+  }
+}
+
+class TimeseriesAssemblyFactory {
+  static TimeseriesAssembly fromJson(core.Map _json) {
+    var message = new TimeseriesAssembly();
+    if (_json.containsKey("analysis")) {
+      message.analysis = core.DateTime.parse(_json["analysis"]);
+    }
+    if (_json.containsKey("editions")) {
+      message.editions = _json["editions"].map((value) => EditionFactory.fromJson(value)).toList();
+    }
+    if (_json.containsKey("node")) {
+      message.node = TimeseriesNodeFactory.fromJson(_json["node"]);
+    }
+    return message;
+  }
+
+  static core.Map toJson(TimeseriesAssembly message) {
+    var _json = new core.Map();
+    if (message.analysis != null) {
+      _json["analysis"] = (message.analysis).toIso8601String();
+    }
+    if (message.editions != null) {
+      _json["editions"] = message.editions.map((value) => EditionFactory.toJson(value)).toList();
+    }
+    if (message.node != null) {
+      _json["node"] = TimeseriesNodeFactory.toJson(message.node);
+    }
+    return _json;
+  }
+}
+
+class TimeseriesBestSeriesFactory {
+  static TimeseriesBestSeries fromJson(core.Map _json) {
+    var message = new TimeseriesBestSeries();
+    if (_json.containsKey("editions")) {
+      message.editions = _json["editions"].map((value) => EditionFactory.fromJson(value)).toList();
+    }
+    if (_json.containsKey("latestAt")) {
+      message.latestAt = core.DateTime.parse(_json["latestAt"]);
+    }
+    if (_json.containsKey("node")) {
+      message.node = TimeseriesNodeFactory.fromJson(_json["node"]);
+    }
+    return message;
+  }
+
+  static core.Map toJson(TimeseriesBestSeries message) {
+    var _json = new core.Map();
+    if (message.editions != null) {
+      _json["editions"] = message.editions.map((value) => EditionFactory.toJson(value)).toList();
+    }
+    if (message.latestAt != null) {
+      _json["latestAt"] = (message.latestAt).toIso8601String();
+    }
+    if (message.node != null) {
+      _json["node"] = TimeseriesNodeFactory.toJson(message.node);
+    }
+    return _json;
+  }
+}
+
+class TimeseriesNodeFactory {
+  static TimeseriesNode fromJson(core.Map _json) {
+    var message = new TimeseriesNode();
+    if (_json.containsKey("element")) {
+      message.element = _json["element"];
+    }
+    if (_json.containsKey("locationName")) {
+      message.locationName = _json["locationName"];
+    }
+    if (_json.containsKey("locationSuffix")) {
+      message.locationSuffix = _json["locationSuffix"];
+    }
+    if (_json.containsKey("model")) {
+      message.model = _json["model"];
+    }
+    if (_json.containsKey("product")) {
+      message.product = _json["product"];
+    }
+    return message;
+  }
+
+  static core.Map toJson(TimeseriesNode message) {
+    var _json = new core.Map();
+    if (message.element != null) {
+      _json["element"] = message.element;
+    }
+    if (message.locationName != null) {
+      _json["locationName"] = message.locationName;
+    }
+    if (message.locationSuffix != null) {
+      _json["locationSuffix"] = message.locationSuffix;
+    }
+    if (message.model != null) {
+      _json["model"] = message.model;
+    }
+    if (message.product != null) {
+      _json["product"] = message.product;
+    }
+    return _json;
+  }
+}
+
---
+++ lib/server/epd_api.dart
@@ -0,0 +1,126 @@
+library epd_api;
+
+import 'dart:async';
+import 'dart:core';
+
+import 'package:rpc/rpc.dart';
+import 'package:logging/logging.dart';
+
+import "../common/timeseries_model.dart";
+import "timeseries_data_cache.dart";
+
+
+//http://localhost:9090/api/discovery/v1/apis
+//http://localhost:9090/api/epd/v1/index
+//http://localhost:9090/api/epd/v1/byAnalysis/City, Town & Spot Forecasts/PDF-PROFOUND/20150215T0300Z?locations=01492.INTL,03266.INTL&elements=TTTTT&validFrom=20150215T0400Z&validTo=20150215T0600Z
+//http://localhost:9090/api/epd/v1/byLatest/City, Town & Spot Forecasts/PDF-PROFOUND/20150215T0300Z/20150215T0600Z?locations=01492.INTL,03266.INTL&elements=TTTTT
+
+//http://localhost:9090/api/epd/v1/byLatest/City,%20Town%20&%20Spot%20Forecasts/PDF-PROFOUND/20150516T0700Z/20150520T0700Z?locations=01492.INTL,03266.INTL&elements=TTTTT
+
+final Logger _log = new Logger('epd_api');
+
+class MyMessage {
+  String message;
+}
+
+@ApiClass(version: 'v1', description: 'Epd Api', name: 'epd')
+class EpdApi {
+  TimeseriesDataCache cache;
+
+  EpdApi();
+  EpdApi.create(this.cache);
+
+  @ApiMethod(method: 'GET', path: 'index')
+  MyMessage index() {
+    return new MyMessage()..message = "index message";
+  }
+
+  @ApiMethod(method: 'GET', path: 'byAnalysis/{product}/{model}/{analysis}')
+  Future<List<TimeseriesAssembly>> byAnalysis(
+      String product, String model, String analysis,
+      {String locations, String elements, String validFrom, String validTo}) {
+    try {
+      if ((locations == null) || (elements == null)) {
+        throw new FormatException(
+            "locations and elements are required query paramters");
+      }
+      product = Uri.decodeComponent(product);
+      model = Uri.decodeComponent(model);
+      analysis = Uri.decodeComponent(analysis);
+
+      List<TimeseriesNode> nodes =
+      _extractNodes(locations, elements, product, model);
+
+      DateTime analysisAt = _parseDateTime(analysis);
+
+      DateTime valid_From = null;
+      Duration period = null;
+      if (validFrom != null) {
+        valid_From = _parseDateTime(validFrom);
+        if (validTo != null) {
+          DateTime valid_To = _parseDateTime(validTo);
+          period = valid_To.difference(valid_From);
+        }
+      }
+
+      return cache.getTimeseriesAnalysisSet(
+          nodes, analysisAt, valid_From, period);
+    } catch (e) {
+      _log.warning("had exception ${e}");
+      throw new ApplicationError(new Exception(e));
+    }
+  }
+
+  @ApiMethod(
+      method: 'GET', path: 'byLatest/{product}/{model}/{validFrom}/{validTo}')
+  Future<List<TimeseriesBestSeries>> byLatest(
+      String product, String model, String validFrom, String validTo,
+      {String locations, String elements}) {
+    try {
+      if ((locations == null) || (elements == null)) {
+        throw new FormatException(
+            "locations and elements are required query paramters");
+      }
+      product = Uri.decodeComponent(product);
+      model = Uri.decodeComponent(model);
+
+      DateTime validFromAt = _parseDateTime(Uri.decodeComponent(validFrom));
+      DateTime validToAt = _parseDateTime(Uri.decodeComponent(validTo));
+      Duration duration = validToAt.difference(validFromAt);
+
+      List<TimeseriesNode> nodes =
+      _extractNodes(locations, elements, product, model);
+
+      return cache.getTimeseriesBestSeriesSet(nodes, validFromAt, duration);
+    } catch (e) {
+      _log.warning("had exception ${e}");
+      throw new ApplicationError(new Exception(e));
+    }
+  }
+
+  List<TimeseriesNode> _extractNodes(
+      String locations, String elements, String product, String model) {
+    List<TimeseriesNode> nodes = [];
+    List<String> locationList = locations.split(",");
+    List<String> elementList = elements.split(",");
+
+    locationList.forEach((location) {
+      elementList.forEach((element) {
+        String locationName = location.split("\.")[0];
+        String locationSuffix = location.split("\.")[1];
+        nodes.add(new TimeseriesNode.create(
+            product, model, element, locationName, locationSuffix));
+      });
+    });
+    return nodes;
+  }
+
+  DateTime _parseDateTime(String time) {
+    try {
+      return DateTime.parse(time);
+    } catch (a) {
+      throw new FormatException(
+          "Time value of '${time}' is invalid, must be ISO time format");
+    }
+  }
+}
