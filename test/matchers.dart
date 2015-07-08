a206b8c49c2e6c07c08d61769195be30d29a0ae6
Richard Schmidt <hangstrap@gmail.com>
2015-07-08 23:07:10 +1200

    trying to get pdf to work

--- test/matchers.dart
+++ test/matchers.dart
@@ -5,10 +5,11 @@ import 'package:test/test.dart';
 ///assumes exception has a field called message
 class _ExceptionWithMessageMatcher extends Matcher {
   final String expectedMessage;
-  final Type expectionType;
-  const _ExceptionWithMessageMatcher(this.expectionType, this.expectedMessage);
+  final Type exceptionType;
+
+  const _ExceptionWithMessageMatcher(this.exceptionType, this.expectedMessage);
   bool matches(item, Map matchState) {
-    if (item.runtimeType == expectionType) {
+    if (item.runtimeType == exceptionType) {
       try {
         return (item.message == expectedMessage);
       } catch (e) {
@@ -18,7 +19,30 @@ class _ExceptionWithMessageMatcher extends Matcher {
     }
     return false;
   }
-  Description describe(Description description) => description.add('${expectionType} with a message of ').addDescriptionOf(expectedMessage);
+
+  Description describe(Description description) => description.add('${exceptionType} with a message of ').addDescriptionOf(expectedMessage);
 }
 Matcher exceptionMatching(type, message) => new _ExceptionWithMessageMatcher(type, message);
 
+class _DoubleMatcher extends Matcher {
+  final num expected;
+  final num error;
+  String err = "";
+
+  _DoubleMatcher(this.expected, this.error);
+
+  bool matches(item, Map matchState) {
+    num diff = item - expected;
+    diff = diff.abs();
+    if (diff > error) {
+      err = "                          diff=${diff}";
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  Description describe(Description description) => description.add("${expected}").addDescriptionOf(err);
+}
+
+Matcher doubleMatcher(num expected, {num error:1.0e-11}) => new _DoubleMatcher(expected, error);
